d90516d910d5a11f8d1af7f6d659b770d6c6f89c
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-04-04 07:32:31 -0500

    creando servicio login

---
+++ src/services/authService.ts
@@ -0,0 +1,10 @@
+import { api } from "../api/api";
+
+export const login = async (correo: string, password: string) => {
+  const response = await api.post("/usuarios/login", {
+    correo,
+    password,
+  });
+
+  return response.data;
+};
