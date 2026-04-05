d3b786f6fd4aa8a2d3290bd6f4ffa0d6ffd35756
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-04-05 10:58:10 -0500

    TypeError: Cannot set property 'href' of undefined

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,48 +8,10 @@ export const api = axios.create({
   baseURL: BASE_URL,
 });
 
-api.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = AsyncStorage.getItem("access");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      const refreshToken = AsyncStorage.getItem("refresh");
-
-      if (!refreshToken) {
-        window.location.href = "/login";
-        return Promise.reject(error);
-      }
-
-      try {
-        const response = await axios.post(`${BASE_URL}/usuarios/login`, {
-          refresh: refreshToken,
-        });
-
-        const newAccessToken = response.data.token;
-
-        await AsyncStorage.setItem("access", newAccessToken);
-
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-        return api(originalRequest);
-      } catch (err) {
-        await AsyncStorage.clear();
-        window.location.href = "/login";
-      }
-    }
-    return Promise.reject(error);
-  },
-);
